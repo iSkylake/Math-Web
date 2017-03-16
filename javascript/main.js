79b9218b9fb6dceae6284a6e36dc6a2936e6ac1b
Llorch <SkylakeBeta@gmail.com>
2017-03-15 23:23:42 -0700

    Add content

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+var op1 = document.getElementById('op1');
+var op2 = document.getElementById('op2');
+var opType = document.getElementById('op-type');
+var answer = document.getElementsByClassName('answer');
+
+function verify(){
+	console.log(answer[0].innerHTML);
+}
+
+// verify();
