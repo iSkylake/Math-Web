18bcf73672b6d28856162abf8fa87f8665116b69
Llorch <SkylakeBeta@gmail.com>
2017-03-16 23:14:55 -0700

    Generate addition

--- javascript/main.js
+++ javascript/main.js
@@ -3,8 +3,19 @@ var op2 = document.getElementById('op2');
 var opType = document.getElementById('op-type');
 var answer = document.getElementsByClassName('answer');
 
-function verify(){
-	console.log(answer[0].innerHTML);
+function verify(a){
+	console.log(a);
+}
+
+function reset(){
+	op1.innerHTML = Math.floor(Math.random()*10);
+	op2.innerHTML = Math.floor(Math.random()*10);
+}
+
+for(i=0; i<answer.length; i++){
+	answer[i].addEventListener("click", function(){
+		verify(this.innerHTML);
+	});
 }
 
 // verify();
